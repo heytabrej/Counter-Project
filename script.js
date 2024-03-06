e9073265bb4a5a79c7182e9074236737324fd60d
Tabrej <116994171+heytabrej@users.noreply.github.com>
2024-03-06 17:30:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const decreasebtn = document.getElementById('decreasebtn');
+const reset = document.getElementById('reset');
+const increasebtn = document.getElementById('incresebtn');
+
+const countlabel = document.getElementById('countlabel');
+
+let count = 0;
+
+
+increasebtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreasebtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+
+reset.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
